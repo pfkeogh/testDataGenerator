6583dc176d4322988790e677a975e006a55813bd
pfkeogh <pkeogh@activecampaign.com>
2020-09-29 07:33:16 -0700

    result printing

--- postman/runner.py
+++ postman/runner.py
@@ -5,15 +5,25 @@ from postman.request import RequestTemplate
 from multiprocessing import Pool
 from functools import partial
 import requests
-
+import time
 
 
 # pass the postman filename and number of requests to make
+# ex: python runner.py <filename> <number of requests>
 def main():
     script, filename, number_of_requests = argv
     postman_json = read_file(filename)
     request_templates = request_builder(postman_json)
+    start_time = time.time()
     multiprocess_runner(request_templates, number_of_requests)
+    end_time = time.time()
+    total_time = end_time - start_time
+    total_requests = len(request_templates)*int(number_of_requests)
+    results = f"{total_requests} requests made in {total_time} seconds. {round(float(total_requests)/total_time, 2)} requests per second\n"
+    print(results)
+    results_file = open('results.txt', 'a')
+    results_file.write(results)
+    results_file.close()
 
 
 def multiprocess_runner(request_templates, number_of_requests):
@@ -23,10 +33,11 @@ def multiprocess_runner(request_templates, number_of_requests):
 
 
 def request_sender(request_template: RequestTemplate, number_of_requests):
-    pid = f"process:{os.getpid()} request: {request_template.name} method: {request_template.method}"
+    pid = f"process: {os.getpid()} request: {request_template.name} method: {request_template.method}"
+    print(f'starting {pid}')
     for i in range(0, int(number_of_requests)):
         requests.post(request_template.url, data=request_template.data, headers=request_template.headers)
-    print(f'{pid} complete')
+    print(f'completed {pid}')
 
 
 def request_builder(postman_json):
